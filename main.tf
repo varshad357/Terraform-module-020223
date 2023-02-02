29ce85ea71c9f950fecc6d0c2f53a33c502920c8
varshad357 <124298660+varshad357@users.noreply.github.com>
2023-02-02 23:56:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,40 @@ provider "azurerm" {
   features{}
 }
 
+data "azurerm_client_config" "current" {}
+
+
+resource "azurerm_resource_group" "myrg1222" {
+  name     = var.resourcegroupname
+  location = var.resourcegrouplocation
+}
+
+resource "azurerm_key_vault" "myakv-ltim" {
+  name                        = var.keyvalutname
+  location                    = azurerm_resource_group.myrg1222.location
+  resource_group_name         = azurerm_resource_group.myrg1222.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
