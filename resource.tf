285ee63c90af91b6e523ff24b97028189a32b9c9
varshad357 <124298660+varshad357@users.noreply.github.com>
2023-02-02 23:52:53 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,6 @@
 module "myakv0202" {
-  source = "./module/akvltim"   # Terraform Local Module - Local Path
+  #source = "./module/akvltim"   # Terraform Local Module - Local Path
+  source = "github.com/varshad357/Terraform-module-020223"                     # Github Clone over HTTPS
   resourcegroupname = var.resourcegroupname
   resourcegrouplocation = var.resourcegrouplocation
   keyvalutname = var.keyvalutname
